abd15b14271109c7382ad55d04c4c41be843681d
Az <122080232+Geymooo@users.noreply.github.com>
2024-08-03 20:44:48 +0700

    Delete vision.js

--- src/components/fragments/about/vision.js
+++
@@ -1,9 +0,0 @@
-export default function Vision() {
-    return (
-      <div className="container mx-auto p-4">
-        <h1 className="text-2xl font-bold">Vision</h1>
-        <p>This is the Vision fragment from src/app/components/fragment/about of your Next.js app.</p>
-      </div>
-    );
-  }
-  
