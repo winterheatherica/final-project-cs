db914bfd6e75f2751d9f6e8239c94fd0d9b66a57
winterheeeeath <winterheatherica@gmail.com>
2024-08-03 13:36:25 +0700

    Initialize

--- src/app/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold">Home Page</h1>
-      <p>Welcome to the Home Page!</p>
-    </div>
-  );
-}
--- src/components/fragments/about/vision.js
+++ src/components/fragments/about/vision.js
@@ -2,7 +2,7 @@ export default function Vision() {
     return (
       <div className="container mx-auto p-4">
         <h1 className="text-2xl font-bold">Vision</h1>
-        <p>This is the Vision fragment from src/app/components/fragment/home of your Next.js app.</p>
+        <p>This is the Vision fragment from src/app/components/fragment/about of your Next.js app.</p>
       </div>
     );
   }
