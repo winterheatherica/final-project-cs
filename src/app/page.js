46609044f712ca3cab4bd103edb24c0306349a92
NazeeraAlthea <ryamaulana2894@gmail.com>
2024-08-06 19:57:13 +0700

    carousel and event

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,10 @@
+"use client"
+
 import Banner from "@/components/fragments/home/banner";
 import RecentEvent from "@/components/fragments/home/recent-event";
 import DivisionButton from "@/components/fragments/home/division-button";
+
+
 
 export default function Home() {
   return (
