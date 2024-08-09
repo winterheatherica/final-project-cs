81a4ea512a941178aa5c65bb0c1f0a86043366cb
NazeeraAlthea <ryamaulana2894@gmail.com>
2024-08-10 00:59:00 +0700

    push

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -1,72 +1,3 @@
-// // src/components/Navbar.js
-// "use client";
-
-// import { useState } from 'react';
-// import Image from 'next/image';
-// import logo from '../../../public/next.svg';
-
-// export default function Navbar() {
-//   const [sidebarOpen, setSidebarOpen] = useState(false);
-
-//   return (
-//     <nav className="bg-[#071135] text-white fixed w-screen">
-//       <div className="container mx-auto flex items-center justify-between p-4">
-//         {/* Logo */}
-//         <div className="flex items-center">
-//           <Image src={logo} alt="Logo" width={40} height={40} />
-//         </div>
-
-//         {/* Navigation Links for Desktop */}
-//         <div className="hidden md:flex space-x-9 justify-center flex-1">
-//           <a href="/" className="hover:text-[#0E1E76]">Home</a>
-//           <a href="/about" className="hover:text-[#0E1E76]">About</a>
-//           <a href="/division" className="hover:text-[#0E1E76]">Division</a>
-//           <a href="/event" className="hover:text-[#0E1E76]">Event</a>
-//           <a href="/contact" className="hover:text-[#0E1E76]">Contact</a>
-//         </div>
-
-//         {/* Login Button for Desktop */}
-//         <div className="hidden md:block">
-//             <a href="/register" className="bg-[#0E1E76] hover:bg-[#030617] text-white px-4 py-2 rounded">Login</a>
-//         </div>
-
-//         {/* Burger Menu for Mobile */}
-//         <div className="md:hidden">
-//           <button onClick={() => setSidebarOpen(true)}>
-//             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-//               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
-//             </svg>
-//           </button>
-//         </div>
-//       </div>
-
-//       {/* Sidebar for Mobile */}
-//       <div
-//         className={`fixed top-0 right-0 h-full bg-[#071135] text-white w-3/5 transform ${
-//           sidebarOpen ? 'translate-x-0' : 'translate-x-full'
-//         } transition-transform duration-300 ease-in-out`}
-//       >
-//         <div className="flex flex-col p-4 space-y-4">
-//           <div className="flex justify-end">
-//             <button onClick={() => setSidebarOpen(false)}>
-//               <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-//                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
-//               </svg>
-//             </button>
-//           </div>
-//           <a href="/" className="hover:text-[#0E1E76]">Home</a>
-//           <a href="/about" className="hover:text-[#0E1E76]">About</a>
-//           <a href="/division" className="hover:text-[#0E1E76]">Division</a>
-//           <a href="/event" className="hover:text-[#0E1E76]">Event</a>
-//           <a href="/contact" className="hover:text-[#0E1E76]">Contact</a>
-//           <button className="bg-[#0E1E76] hover:bg-[#030617] text-white px-4 py-2 rounded mt-4">
-//             <a href="/register" className="bg-[#0E1E76] hover:bg-[#030617] text-white px-4 py-2 rounded">Login</a>
-//           </button>
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// }
 "use client";
 
 import { useState, useEffect } from 'react';
@@ -102,10 +33,11 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="bg-[#071135] text-white fixed w-screen">
+    <nav className="bg-[#071135] text-white fixed w-screen z-10">
       <div className="container mx-auto flex items-center justify-between p-4">
-        <div className="flex items-center">
-          <Image src={logo} alt="Logo" width={40} height={40} />
+        <div className="flex items-center gap-1 font-medium">
+          <img src="/logo.png" className="h-8"/>
+          <p>CSC PNJ</p>
         </div>
         <div className="hidden md:flex space-x-9 justify-center flex-1">
           {user?.type === 'A' && <Link href="/dashboard" className="hover:text-[#0E1E76]">Dashboard</Link>}
@@ -122,7 +54,7 @@ export default function Navbar() {
               <button onClick={handleLogout} className="bg-[#0E1E76] hover:bg-[#030617] text-white px-4 py-2 rounded">Logout</button>
             </div>
           ) : (
-            <Link href="/login" className="bg-[#0E1E76] hover:bg-[#030617] text-white px-4 py-2 rounded">Login</Link>
+            <Link href="/login" className="border border-white hover:bg-white hover:text-[#030617] text-white px-4 py-2 rounded">Login</Link>
           )}
         </div>
         <div className="md:hidden">
@@ -135,7 +67,7 @@ export default function Navbar() {
       </div>
       <div className={`fixed top-0 right-0 h-full bg-[#071135] text-white w-3/5 transform ${sidebarOpen ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-300 ease-in-out`}>
         <div className="flex flex-col p-4 space-y-4">
-          <div className="flex justify-end">
+          <div className="flex justify-end w-full">
             <button onClick={() => setSidebarOpen(false)}>
               <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
@@ -148,14 +80,14 @@ export default function Navbar() {
           <Link href="/division" className="hover:text-[#0E1E76]">Division</Link>
           <Link href="/event" className="hover:text-[#0E1E76]">Event</Link>
           {user?.type !== 'A' && <Link href="/contact" className="hover:text-[#0E1E76]">Contact</Link>}
-          <div className="mt-4">
+          <div className="mt-4 flex">
             {user ? (
               <div className="flex flex-col items-start space-y-4">
                 <span>Halo, {user.username}</span>
                 <button onClick={handleLogout} className="bg-[#0E1E76] hover:bg-[#030617] text-white px-4 py-2 rounded">Logout</button>
               </div>
             ) : (
-              <Link href="/login" className="bg-[#0E1E76] hover:bg-[#030617] text-white px-4 py-2 rounded">Login</Link>
+              <Link href="/login" className="border border-white hover:bg-white hover:text-[#030617] text-white px-4 py-2 rounded w-full text-center">Login</Link>
             )}
           </div>
         </div>
